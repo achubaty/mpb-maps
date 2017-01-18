bd1544df50812dd1708111cb8edd9197e14553be
Alex Chubaty <alex.chubaty@gmail.com>
2017-01-18 08:37:15 -0800

    create rasters of MPB AB aerial survey data
    sum NUM_TREES per 4km^2 cell, per Cooke and Carrol (unpublished)

--- 2016_AB_MPB_aerial_survey_data.R
+++ 2016_AB_MPB_aerial_survey_data.R
@@ -1,20 +1,74 @@
 library(magrittr)
 library(rgdal)
+library(snowfall)
 library(sp)
 library(stringr)
 
-# check that gdal driver exists to open gdb file
-if (NROW(subset(ogrDrivers(), grepl("GDB", name))) == 0) {
-  warning('The OpenFileGDB driver is not available on your system.')
+## compute environment variables
+._CFS_. <- grepl("W-VIC", Sys.info()[["nodename"]])  ## logical: is this a CFS machine?
+._NUM_CPUS_. <- parallel::detectCores() / 2          ## max cpus to use
+._OS_. <- Sys.info()[["sysname"]]
+._USER_. <- Sys.info()[["user"]]
+
+## MPB data processing options
+._RES_MAPS_. <- 2000  ## raster resolution, based on Cooke & Carroll (unpublished)
+
+## set R and knitr options
+if (isTRUE(._CFS_.)) {
+  options(repos = c(CRAN = "https://cran.rstudio.com/",
+                    NRCRAN = "http://132.156.149.95"))
+} else {
+  options(repos = c(CRAN = "https://cran.rstudio.com/"))
 }
 
-##  file originally downlooaed from:
+## set work dirs based on computer used
+if (._USER_. == "achubaty") {
+  if (._OS_. == "Darwin") {
+    maps.dir <- "~/Documents/data/maps"
+    work.dir <- "~/Documents/GitHub/MPB/mpb-maps"
+  } else if (._OS_. == "Linux") {
+    if (isTRUE(._CFS_.)) {
+      maps.dir <- "/mnt/A105388/shared/data"
+    } else {
+      maps.dir <- "~/Documents/Data/shared"
+    }
+    work.dir <- "~/Documents/GitHub/MPB/mpb-maps"
+  } else if (._OS_. == "Windows") {
+    maps.dir <- "//W-VIC-A105388/shared/data"
+    work.dir <- "~/GitHub/MPB/mpb-maps"
+  } else {
+    stop("Which operating system are you using?")
+  }
+}
+setwd(work.dir)
+rdata.path <- file.path(maps.dir, "MPB", "Rmaps")
+fig.path <- file.path(work.dir, "figures")
+if (!dir.exists(fig.path)) dir.create(fig.path)
+
+stopifnot(all(dir.exists(c(fig.path, maps.dir, rdata.path, work.dir))))
+
+# check that gdal driver exists to open gdb file
+if (NROW(subset(ogrDrivers(), grepl("GDB", name))) == 0) {
+  stop('The OpenFileGDB driver is not available on your system.')
+}
+
+## helper functions:
+
+# imports 'dl.data' to provide robust downloading with file checksumming
+source_url("https://raw.githubusercontent.com/achubaty/r-tools/master/download-data.R")
+
+#' manual garbage collection to free recently unallocated memory 
+.cleanup <- function() {
+  for (i in 1:10) gc()
+}
+
+##  file originally downloaded from:
 ##    ftp://ftp.env.gov.ab.ca/pub/MPB/Data/MPB_AERIAL_SURVEY.gdb.7z
 ##
 ##  [ password protected archive, downloaded October 19, 2016                       ]
 ##  [ see email from Aaron McGill (Aaron.McGill@gov.ab.ca) forwarded by Barry Cooke ]
 ##
-file <- '//W-VIC-A105388/shared/data/MPB/MPB_AERIAL_SURVEY.gdb'
+file <- file.path(maps.dir, "MPB/MPB_AERIAL_SURVEY.gdb")
 
 # List all feature classes in a file geodatabase
 fc_list <- ogrListLayers(file)
@@ -36,13 +90,71 @@ lapply(fc_list, function(x) {
 # Determine the FC extent, projection, and attribute information
 lapply(layers, summary)
 
-# load boreal.west (intersection of boreal forest with AB, BC, SK)
-load('//W-VIC-A105388/shared/data/MPB/Rmaps/boreal.west.RData')
+# Extract points and polygon data into separate lists
+id_pnts <- which(substring(names(layers), 5, 5) == "x")
+id_poly <- which(substring(names(layers), 5, 5) == "p")
 
-# reproject boreal.west to match the projection of the mpb layer
-boreal.west.2 <- spTransform(boreal.west, CRSobj = proj4string(layers[[1]]))
+ab_pnts <- layers[id_pnts]
+ab_poly <- layers[id_poly]
+
+# order by year
+ab_pnts <- ab_pnts[order(names(ab_pnts))]
+ab_poly <- ab_poly[order(names(ab_poly))]
+
+stopifnot(all(sapply(ab_pnts, is, class2 = "SpatialPointsDataFrame")))
+stopifnot(all(sapply(ab_poly, is, class2 = "SpatialPolygonsDataFrame")))
+rm(layers)
+
+# reproject the MPB maps to match the boreal.west projection
+# (boreal.west is the intersection of boreal forest with AB, BC, SK)
+if (!exists("crs.boreal")) {
+  load(file.path(rdata.path, "boreal.west.RData"))
+  crs.boreal <- CRS(proj4string(boreal.west))
+}
+
+sfInit(cpus = ._NUM_CPUS_., parallel = TRUE)
+  sfLibrary(rgdal)
+  
+  ab_pnts_boreal <- sfClusterApplyLB(ab_pnts, spTransform, crs.boreal) %>%
+    setNames(names(ab_pnts))
+
+  # save these new map objects for later use
+  save("ab_pnts_boreal", file = file.path(rdata.path, "ab_pnts_boreal.Rdata"))
+sfStop()
+.cleanup()
 
 # plot them
 dev.new(noRStudioGD = TRUE)
-plot(boreal.west.2)
-lapply(layers, plot, add = TRUE, col = 'darkred')
+plot(boreal.west)
+lapply(ab_pnts_boreal, plot, add = TRUE, col = 'darkred')
+
+## RASTERIZE
+load(file.path(rdata.path, "west.boreal.Rdata"))
+west.empty.raster <- raster(extent(west.boreal), resolution = ._RES_MAPS_.)
+west.boreal.raster <- suppressWarnings(
+  rasterize(west.boreal, west.empty.raster,
+            filename = file.path(rdata.path, "west_boreal.grd"),
+            overwrite = TRUE)
+)
+save("west.boreal.raster", file = file.path(rdata.path, "west.boreal.raster.Rdata"))
+
+# rasterize MPB point data per method oulined in Cooke & Carrol (unpublished)
+rasterize.mpb <- function(x, y = west.boreal.raster, ...) {
+  rasterize(x = x, y = y,
+            field = as.numeric(x@data$NUM_TREES),
+            fun = "sum", overwrite = TRUE, ...)
+}
+
+sfInit(cpus = ._NUM_CPUS_., parallel = TRUE)
+  sfLibrary(raster)
+  sfExport("west.boreal.raster")
+  
+  ab_pnts_boreal_stack <- stack(sfClusterApplyLB(ab_pnts_boreal, rasterize.mpb)) %>%
+    setNames(names(ab_pnts_boreal))
+  writeRaster(ab_pnts_boreal_stack, file.path(rdata.path, "ab_pnts_boreal.grd"), overwrite = TRUE)
+  save("ab_pnts_boreal_stack", file = file.path(rdata.path, "ab_pnts_boreal_stack.Rdata"))
+sfStop()
+.cleanup()
+
+dev.new(noRStudioGD = TRUE)
+plot(ab_pnts_boreal_stack)
